bd9e2946dcde144e9c2d12dc28484a7e8a2bf4a5
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2021-01-06 01:07:09 +0700

    update from laptop 04

--- src/php/template-parts/content.php
+++ src/php/template-parts/content.php
@@ -8,20 +8,21 @@
  */
 
 ?>
-<article id="post-<?php the_ID(); ?>">
-    <div class="row">
-        <div class="col-md-3 noRight">
-            <?php echo the_post_thumbnail('my-custom-thumb'); ?>
-        </div>
-        <div class="col-md-9">
-            <?php the_title( '<h4 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' );?>
-            <p><small><?php the_time("d M Y"); ?> by <?php the_author() ?></small></p>
-            <p><?php the_excerpt(); ?></p>
-            <div class="morelink">
-                <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" >Читать далее…</a>
-            </div>
+<article id="post-<?php the_ID(); ?>" class="row">
 
-        </div>
+    <div class="col-md-3 noRight">
+        <?php echo the_post_thumbnail('my-custom-thumb'); ?>
     </div>
+    <div class="col-md-9">
+        <?php the_title( '<h4 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' );?>
+        <span class="badge badge-primary cat-link"><?php the_category(', '); ?></span>
+        <p><small><?php the_time("d M Y"); ?> by <?php the_author() ?></small></p>
+        <p><?php the_excerpt(); ?></p>
+        <div class="morelink">
+            <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" >Читать далее…</a>
+        </div>
+
+    </div>
+
     <div class="borderTop mt-3"></div>
 </article><!-- #post-<?php the_ID(); ?> -->
